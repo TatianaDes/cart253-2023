770c737626ab9f8985790622e988f04ad1359673
TatianaDes <tatiana.d0714@gmail.com>
2023-09-06 20:09:17 -0400

    Drawing experiment. Drew a cone

---
+++ experiments/drawing experiment/drawing experiment/js/script.js
@@ -0,0 +1,43 @@
+/**
+ * Title of Project
+ * Author Name
+ *
+ * This is a template. You must fill in the title, author,
+ * and this description to match your project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+  createCanvas(500, 500);
+
+  background(255, 138, 1);
+
+  ellipseMode(CORNER);
+  ellipse(250, 250, 100, 100);
+  ellipse(250, 250, 80, 80);
+  ellipse(250, 250, 60, 60);
+  ellipse(250, 250, 40, 40);
+  ellipse(250, 250, 20, 20);
+
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
