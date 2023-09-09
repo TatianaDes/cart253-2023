0ad2924e196a84b735bc32aa4ff0290fd98f3a4c
TatianaDes <tatiana.d0714@gmail.com>
2023-09-08 23:50:20 -0400

    Activity 02: Drew my alien

--- experiments/02-draw-an-alien/js/script.js
+++ experiments/02-draw-an-alien/js/script.js
@@ -1,9 +1,10 @@
 /**
- * Title of Project
- * Author Name
+ * Activity 02: Draw an Alien
+ * Tatiana Désormeaux
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Drawing an alien in JavaScript using all the basic knowledge we have learned thus far. 
+ * 
+ * Currently draws body, head, and face of an alien.
  */
 
 "use strict";
@@ -17,15 +18,42 @@ function preload() {
 
 
 /**
- * Description of setup
+ * Draws an alien on the canvas
 */
 function setup() {
 
+    createCanvas(640, 480);
+
+    background(255, 100, 100);
+    noStroke();
+
+    // Draw the body
+    fill(127);
+    ellipse(320, 480, 300, 200);
+
+    // Draw the head
+    fill(100);
+    ellipse(320, 240, 250, 400);
+
+    // Draw the eyes
+    fill(0);
+    ellipse(250, 240, 80, 250);
+    ellipse(390, 240, 80, 250);
+
+    // Draw the nostrills
+    ellipse(300, 350, 10, 10);
+    ellipse(340, 350, 10, 10);
+
+    // Draw the mouth
+    stroke(200, 0, 0);
+    strokeWeight(5);
+    rectMode(CENTER);
+    rect(320, 390, 100, 25);
 }
 
 
 /**
- * Description of draw()
+ * Does nothing
 */
 function draw() {
 
