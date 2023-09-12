24c4d3ed99fe3ac1034fd0495c102f82e044d04c
TatianaDes <tatiana.d0714@gmail.com>
2023-09-12 11:05:56 -0400

    Activity 02: Tweaked my alien to make it my own

--- experiments/02-draw-an-alien/js/script.js
+++ experiments/02-draw-an-alien/js/script.js
@@ -24,31 +24,31 @@ function setup() {
 
     createCanvas(640, 480);
 
-    background(255, 100, 100);
-    noStroke();
+    background(252, 3, 127);
 
     // Draw the body
-    fill(127);
-    ellipse(320, 480, 300, 200);
+    noStroke();
+    fill(150);
+    ellipse(320, 480, 300, 400);
 
     // Draw the head
-    fill(100);
-    ellipse(320, 240, 250, 400);
+    fill(120);
+    ellipse(320, 240, 400, 250);
 
     // Draw the eyes
     fill(0);
-    ellipse(250, 240, 80, 250);
-    ellipse(390, 240, 80, 250);
+    ellipse(230, 240, 80, 180);
+    ellipse(410, 240, 80, 180);
 
     // Draw the nostrills
-    ellipse(300, 350, 10, 10);
-    ellipse(340, 350, 10, 10);
+    ellipse(300, 310, 10, 10);
+    ellipse(340, 310, 10, 10);
 
     // Draw the mouth
     stroke(200, 0, 0);
-    strokeWeight(5);
+    strokeWeight(3);
     rectMode(CENTER);
-    rect(320, 390, 100, 25);
+    rect(320, 340, 125, 25);
 }
 
 
