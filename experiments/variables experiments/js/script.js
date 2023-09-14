6acf49d6d84c3bdfc68bc668ba9e150cc99809af
TatianaDes <tatiana.d0714@gmail.com>
2023-09-13 21:36:34 -0400

    Learned how to include JavaScript Objects into our program

--- experiments/variables experiments/js/script.js
+++ experiments/variables experiments/js/script.js
@@ -9,11 +9,13 @@
 "use strict";
 
 let backgroundShade = 0;
-let circleX = 0;
-let circleY = 250;
-let circleSize = 200;
-let circleSpeed = 2;
-let circleAcceleration = 0.25;
+
+let circle = {
+    x: 0,
+    y: 250,
+    size: 200,
+    speed: 2
+};
 
 /**
  * Description of preload
@@ -37,8 +39,7 @@ function setup() {
 */
 function draw() {
     background(backgroundShade);
-    circleX += circleSpeed;
-    circleSpeed += circleAcceleration;
-    ellipse(circleX, circleY, circleSize);
+    circle.x += circle.speed;
+    ellipse(circle.x, circle.y, circle.size);
 
 }
