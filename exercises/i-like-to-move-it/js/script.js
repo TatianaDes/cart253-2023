c4e9270e2c399a5776f8ece83d027f5f18c9a7d0
TatianaDes <tatiana.d0714@gmail.com>
2023-09-17 01:22:25 -0400

    E1: Added colour changes, size changes, and mouse movement

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -17,14 +17,18 @@ let circle1 = {
     y: 450,
     size: 100,
     fill: 255,
-    alpha: 200
 };
 let circle2 = {
     x: 500,
     y: 50,
     size: 100,
     fill: 255,
-    alpha: 200
+};
+let circle3 = {
+    x: 255,
+    y: 255,
+    size: 100,
+    fill: 255,
 };
 
 
@@ -41,8 +45,7 @@ function preload() {
 */
 function setup() {
 createCanvas(500, 500);
-noStroke();
-
+    noStroke();
 }
 
 
@@ -55,15 +58,29 @@ background(bg.r, bg.g, bg.b);
 bg.b = bg.b + 1;
 
     //Left circle
+    circle1.size = map(mouseY, 0, height, 10, 95);
     circle1.x = circle1.x + 2;
     circle1.x = constrain(circle1.x, 0, width);
-    fill(circle1, fill, circle1.alpha);
+    fill(235, 228, 156);
     ellipse(circle1.x, circle1.y, circle1.size);
 
     //Right circle
+    circle2.size = map(mouseY, 0, height, 10, 95);
     circle2.x = circle2.x + -2;
     circle2.x = constrain(circle2.x, 0, width);
-    fill(circle2, fill, circle2.alpha);
+    fill(237, 134, 248);
     ellipse(circle2.x, circle2.y, circle2.size);
+
+    //Center circle
+    circle3.fill = map(mouseX, 0, width, 0, 255);
+    fill(circle3.fill);
+    circle3.size = circle3.size + 2;
+    circle3.size = constrain(circle3.size, 0, 300);
+    ellipse(circle3.x, circle3.y, circle3.size);
+
+     //Rectangle center
+     rectMode(CENTER);
+     rect(255, 255, 300, 300);
+     fill(166, 169, 229);
 
 }
