969c67955c1f890675bf2e06d16e3d1e9771f53d
TatianaDes <tatiana.d0714@gmail.com>
2023-09-23 19:09:44 -0400

    Learned how to use booleans

--- experiments/conditionals experiment/js/script.js
+++ experiments/conditionals experiment/js/script.js
@@ -1,51 +1,20 @@
-/**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
+let displayCircle = false;
 
-"use strict";
-
-let backgroundShade = 0;
-let circle = {
-    x: 0,
-    y: 250,
-    size: 100,
-    speed: 1
-}
-
-/**
- * Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
- * Description of setup
-*/
 function setup() {
     createCanvas(500, 500);
 }
 
-
-/**
- * Description of draw()
-*/
 function draw() {
-    background(backgroundShade);
 
-    circle.x = circle.x + circle.speed;
+    background(0);
 
-    fill(255, 255, 255);
+    if (mouseIsPressed) {
+        displayCircle = true;
+    }
 
-    if (!(circle.x < width/3)) {
-            fill(255, 0, 0);
-        }
+    if (displayCircle) {
+        ellipse(250, 250, 100, 100);
+    }
 
-    ellipse(circle.x, circle.y, circle.size);
 }
 
