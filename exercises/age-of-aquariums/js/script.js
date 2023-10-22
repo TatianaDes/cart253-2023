f4154f507c5fde66f2aebeb8f0b993947968efa6
TatianaDes <tatiana.d0714@gmail.com>
2023-10-22 01:51:51 -0400

    E4: Started age of aquarium, added a title, name, and description idea

---
+++ exercises/age-of-aquariums/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Play Time
+ * Tatiana Désormeaux
+ * 
+ * Use the fish aquarium for the dog random movements, make the user another dog and when the dogs come close to the user, they run away/around
+ */
+
+"use strict";
+
+
+function preload() {
+
+}
+
+
+function setup() {
+
+}
+
+
+function draw() {
+
+}
