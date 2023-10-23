58d6c8febe9f1db61c39ad7e58a03e6d2cb4b175
TatianaDes <tatiana.d0714@gmail.com>
2023-10-22 20:32:17 -0400

    E4: Added comments

--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -24,11 +24,13 @@ function preload() {
 function setup() {
     createCanvas(600, 600);
 
+    // Make the fish have random positions and clean up the code
     for (let i = 0; i < schoolSize; i++) {
         school[i] = createFish(random(0, width), random(0, height));
     }
 }
 
+// Creating the fish object for all the fish
 function createFish(x, y) {
     let fish = {
         x: x,
@@ -47,17 +49,20 @@ function draw() {
     moveDog();
     displayDog();
 
+    // Calling the fuctions for all the fish in a clean way
     for (let i = 0; i < school.length; i++) {
         moveFish(school[i]);
         displayFish(school[i]);
     }
 }
 
+// Move dog with the mouse
 function moveDog() {
     dog.x = mouseX;
     dog.y = mouseY;
 }
 
+// Move fish randomly
 function moveFish(fish) {
     let change = random(0, 1);
     if (change < 0.05) {
@@ -65,13 +70,16 @@ function moveFish(fish) {
         fish.vy = random(-fish.speed, fish.speed);
     }
 
+// Position is being added onto the velocity of fish
 fish.x = fish.x + fish.vx;
 fish.y = fish.y + fish.vy;
 
+// Constrain the fish to the width and height of the canvas
 fish.x = constrain(fish.x, 0, width);
 fish.y = constrain(fish.y, 0, height);
 }
 
+// Display the dog
 function displayDog() {
     push();
     fill(255);
@@ -79,6 +87,7 @@ function displayDog() {
     pop(); 
     }
 
+// Display the fish
 function displayFish(fish) {
     push();
     fill(200, 100, 100);
