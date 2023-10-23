c02d2127cdbd1232ed8c8cebb08e870b28dc2a29
TatianaDes <tatiana.d0714@gmail.com>
2023-10-22 20:15:26 -0400

    E4: Created a dog as the main user and used the coding for the aquarium to make the other objects move around

--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -7,6 +7,14 @@
 
 "use strict";
 
+let dog = {
+    x: 0,
+    y: 0,
+    size: 100
+  }
+
+let school = [];
+let schoolSize = 4;
 
 function preload() {
 
@@ -14,10 +22,67 @@ function preload() {
 
 
 function setup() {
+    createCanvas(600, 600);
 
+    for (let i = 0; i < schoolSize; i++) {
+        school[i] = createFish(random(0, width), random(0, height));
+    }
 }
 
+function createFish(x, y) {
+    let fish = {
+        x: x,
+        y: y,
+        size: 50,
+        vx: 0,
+        vy: 0,
+        speed: 2
+    };
+    return fish;
+}
 
 function draw() {
+    background(0);
 
+    moveDog();
+    displayDog();
+
+    for (let i = 0; i < school.length; i++) {
+        moveFish(school[i]);
+        displayFish(school[i]);
+    }
+}
+
+function moveDog() {
+    dog.x = mouseX;
+    dog.y = mouseY;
+}
+
+function moveFish(fish) {
+    let change = random(0, 1);
+    if (change < 0.05) {
+        fish.vx = random(-fish.speed, fish.speed);
+        fish.vy = random(-fish.speed, fish.speed);
+    }
+
+fish.x = fish.x + fish.vx;
+fish.y = fish.y + fish.vy;
+
+fish.x = constrain(fish.x, 0, width);
+fish.y = constrain(fish.y, 0, height);
+}
+
+function displayDog() {
+    push();
+    fill(255);
+    ellipse(dog.x, dog.y, dog.size);
+    pop(); 
+    }
+
+function displayFish(fish) {
+    push();
+    fill(200, 100, 100);
+    noStroke();
+    ellipse(fish.x, fish.y, fish.size);
+    pop();
 }
