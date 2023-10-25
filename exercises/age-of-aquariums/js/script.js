6e1c2eb23dfcf2438e9f60decee5657cfae4c515
TatianaDes <tatiana.d0714@gmail.com>
2023-10-24 22:46:45 -0400

    E4: Changed the colours and spacing of the ending states

--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -2,7 +2,7 @@
  * Play Time
  * Tatiana Désormeaux
  * 
- * Use the fish aquarium for the dog random movements, make the user another dog and when the dogs come close to the user, they run away/around
+ * A program about a big dog scaring off little puppies from eating the dog food.
  */
 
 "use strict";
@@ -12,7 +12,8 @@ let grassImage;
 let dog = {
     x: 0,
     y: 0,
-    size: 50,
+    sizeX: 120,
+    sizeY: 110,
     vx: 0,
     vy: 0,
     speed: 5,
@@ -21,6 +22,7 @@ let dog = {
 
 let litter = [];
 let litterSize = 4;
+//let displayImage;
 
 let food = [];
 let foodSize = 1;
@@ -30,11 +32,11 @@ let state = `title`; // Can be: title, simulation, dog, puppies
 function preload() {
     grassImage = loadImage("assets/images/stardewvalleygrass.jpg");
     dog.image = loadImage("assets/images/labrador.png");
-    /*puppy1.image = loadImage("assets/images/puppy.png");
-    puppy2.image = loadImage("assets/images/puppy2.png");
-    puppy3.image = loadImage("assets/images/puppy3.png");
-    puppy4.image = loadImage("assets/images/puppy4.png");*/
-    dogFood.image = loadImage("assets/images/dogfood(1).png");
+    //litter[0] = loadImage("assets/images/puppy.png");
+    //litter[1] = loadImage("assets/images/puppy2.png");
+    //litter[2] = loadImage("assets/images/puppy3.png");
+    //litter[3] = loadImage("assets/images/puppy4.png");
+    //foodimages[0] = loadImage("assets/images/dogfood(1).png");
 
 }
 
@@ -45,6 +47,7 @@ function setup() {
     // Make the puppies have random positions and clean up the code
     for (let i = 0; i < litterSize; i++) {
         litter[i] = createPuppies(random(0, width), random(0, height));
+        //displayImage = random(litter);
     }
 
     for (let i = 0; i < foodSize; i++) {
@@ -100,17 +103,17 @@ function draw() {
 function title() {
     // Title state
     push();
-    background(0);
+    background(186,209,252);
     textSize(60);
-    fill(238,159,19);
+    fill(7,33,78);
     textAlign(CENTER, CENTER);
     text(`Play Time!`, windowWidth/2, windowHeight/2);
     textSize(20);
-    fill(240,141,63);
+    fill(231,96,84);
     text(`(Press Any Key to Start)`, windowWidth/2, 350);
     textSize(15);
-    fill(244,104,84);
-    text(`Use the arrow keys to move the dog, and click the mouse to drop the food.`, 1100, 570);
+    fill(57,160,100);
+    text(`Use the arrow keys to move the dog towards the puppies and food, and click the mouse to drop the food.`, 1000, 570);
     pop();
 }
 
@@ -136,23 +139,23 @@ function simulation() {
 
 function dogEat() {
     push();
-    background(0);
+    background(255,110,110);
     textSize(60);
-    fill(238,159,19);
+    fill(192,9,9);
     textAlign(CENTER, CENTER);
     text(`You Ate!`, windowWidth/2, windowHeight/2);
-    text(`But at what cost? Now the puppies are hungry. :(`, windowWidth/2, 350);
+    text(`But at what cost? Now the puppies are hungry. :(`, windowWidth/2, 360);
     pop();
 }
 
 function puppiesEat() {
     push();
-    background(0);
-    textSize(60);
-    fill(238,159,19);
+    background(248,248,89);
+    textSize(50);
+    fill(159,113,49);
     textAlign(CENTER, CENTER);
     text(`The Puppies Ate!`, windowWidth/2, windowHeight/2);
-    text(`Thank you for being kind and letting the puppies eat first! :)`, windowWidth/2, 350);
+    text(`Thank you for being kind and letting the puppies eat first! :)`, windowWidth/2, 360);
     pop();
 }
 
@@ -192,7 +195,7 @@ function moveDog() {
 // Make the puppies run away from the dog
 function movePuppies(dogFood, puppies) {
     let a = dist(dog.x, dog.y, puppies.x, puppies.y);
-    if (a < dog.size/2 + puppies.size/2 + 300) {
+    if (a < dog.sizeX/2 + puppies.size/2 + 300) {
          if (dog.x < puppies.x) {
             puppies.ax = puppies.acceleration;
         }
@@ -249,7 +252,7 @@ puppies.y = constrain(puppies.y, 0, height);
 function checkOverlap(dogFood, puppies) {
     // Check if dog and dogFood overlap
     let d = dist(dog.x, dog.y, dogFood.x, dogFood.y);
-    if (d < dog.size/2 + dogFood.size/2) {
+    if (d < dog.sizeX/2 + dogFood.size/2) {
        state = `dogAte`;
     }
     
@@ -266,17 +269,17 @@ function checkOverlap(dogFood, puppies) {
 // Display the dog
 function displayDog() {
     push();
-    fill(255);
-    image(dog.image, dog.x, dog.y, dog.size, dog.size);
+    imageMode(CENTER);
+    image(dog.image, dog.x, dog.y, dog.sizeX, dog.sizeY);
     pop(); 
     }
 
 // Display the puppies
 function displayPuppies(puppies) {
     push();
-    fill(200, 100, 100);
-    noStroke();
-    ellipse(puppies.x, puppies.y, puppies.size);
+    fill(255);
+    //imageMode(CENTER);
+    /*image*/ ellipse(/*displayImage,*/ puppies.x, puppies.y, puppies.size);
     pop();
 }
 
@@ -285,7 +288,7 @@ function displayFood(dogFood) {
     if (!food.eaten) {
         push();
         fill(255);
-        image(dogFood.image, dogFood.x, dogFood.y, dogFood.size);
+        ellipse(dogFood.x, dogFood.y, dogFood.size);
         pop();
     }
 }
