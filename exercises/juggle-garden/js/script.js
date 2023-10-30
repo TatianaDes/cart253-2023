96cd00fc79cade374e0f804a4aecc7b84ce35cee
TatianaDes <tatiana.d0714@gmail.com>
2023-10-30 00:54:49 -0400

    E5: Started juggle garden exercise and added a title, name and description

---
+++ exercises/juggle-garden/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Beautiful Ecosystem
+ * Tatiana Désormeaux
+ * 
+ * Adding onto the garden simulation and making it more user-friendly
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
