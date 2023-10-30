ceab3464411f9a283df9db67e9501c500660e82f
TatianaDes <tatiana.d0714@gmail.com>
2023-10-29 20:30:24 -0400

    Finished constructors with parameters

--- experiments/object-oriented-programming experiment/js/script.js
+++ experiments/object-oriented-programming experiment/js/script.js
@@ -6,7 +6,7 @@ let garden = {
     // An array to store the individual flowers
     flowers: [],
     // How many flowers in the garden
-    numFlowers: 100,
+    numFlowers: 30,
     // The color of the grass (background)
     grassColor: {
       r: 120,
@@ -21,8 +21,17 @@ function setup() {
   
     // Create our flowers by counting up to the number of the flowers
     for (let i = 0; i < garden.numFlowers; i++) {
-      // Create a new flower
-      let flower = new Flower();
+        let x = random(0, width);
+        let y = random(0, height);
+        let size = random(50, 80);
+        let stemLength = random(50, 100);
+        let petalColor = {
+            r: random(100, 255),
+            g: random(100, 255),
+            b: random(100, 255)
+        }
+        // Create a new flower
+      let flower = new Flower(x, y, size, stemLength, petalColor);
       // Add the flower to the array of flowers
       garden.flowers.push(flower);
     }
