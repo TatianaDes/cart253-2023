6b75f4158bbffece1933404c14041d487179d9ba
TatianaDes <tatiana.d0714@gmail.com>
2023-11-10 20:52:20 -0500

    A8: Finished the sound activity

--- activities/08-sound-activity/js/script.js
+++ activities/08-sound-activity/js/script.js
@@ -1,12 +1,17 @@
 /**
- * Title of Project
- * Author Name
+ * Musical Toy
+ * Tatiana Désormeaux
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * A program that plays music based on primitive physics.
  */
 
 "use strict";
+
+// The balls
+let balls = [];
+
+// F-minor
+let notes = [`F3`, `G3`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F4`];
 
 /**
  * Description of preload
@@ -16,11 +21,11 @@ function preload() {
 }
 
 
-/**
- * Description of setup
-*/
+// setup() just creates the canvas.
 function setup() {
+    createCanvas(600, 600);
 
+    userStartAudio();
 }
 
 
@@ -28,5 +33,22 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
 
+    for (let i = 0; i < balls.length; i++) {
+        let ball = balls[i];
+        ball.move();
+        ball.bounce();
+        ball.display();
+    }
+}
+
+function mousePressed() {
+    createBall(mouseX, mouseY);
+}
+
+function createBall(x, y) {
+    let note = random(notes);
+    let ball = new Ball(x, y, note);
+    balls.push(ball);
 }
