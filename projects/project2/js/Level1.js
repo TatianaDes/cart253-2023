700ff5e063d9bfc09f6cf9586ac758e54343bcf1
TatianaDes <tatiana.d0714@gmail.com>
2023-11-17 22:38:00 -0500

    P2: Created a transition from the note1 state to the  level2 state and added my players

--- projects/project2/js/Level1.js
+++ projects/project2/js/Level1.js
@@ -1,12 +1,8 @@
 class Level1 {
 
-    // Creating dimensions of the player
-    constructor(/*{ x, y, w, h, red, green, blue, leftKey, rightKey, upKey, downKey }*/) {
-        this.player = {
-            // x: x,
-            // y: y,
-            // w: w,
-            // h: h,
+    // Creating dimensions of the objects
+    constructor() {
+        this.player = new Player({
             x: 60,
             y: 300,
             w: 30,
@@ -17,92 +13,124 @@ class Level1 {
             leftKey: LEFT_ARROW,
             rightKey: RIGHT_ARROW,
             upKey: UP_ARROW,
-            downKey: DOWN_ARROW,
-            size: 40,
+            downKey: DOWN_ARROW
+        });
+        this.player2 = new Player({
+            x: 90,
+            y: 380,
+            w: 50,
+            h: 25,
+            red: 109,
+            green: 82,
+            blue: 34,
+            leftKey: 65,
+            rightKey: 68,
+            upKey: 87,
+            downKey: 83,
+        });
 
-            speed: 5,
-            vx: 0,
-            vy: 0,
-            ax: 0,
-            ay: 0,
-
-            // r: red,
-            // g: green,
-            // b: blue,
-
-            // leftKey: leftKey,
-            // rightKey: rightKey,
-            // upKey: upKey,
-            // downKey: downKey
-        }
+        this.house = new House({
+            x: 700,
+            y: 500,
+            w: 50,
+            h: 80,
+            red: 113,
+            green: 61,
+            blue: 244,
+            doorKnobSize: 8,
+        });
+        this.house2 = new House({
+            x: 200,
+            y: 150,
+            w: 50,
+            h: 80,
+            red: 43,
+            green: 165,
+            blue: 79,
+            doorKnobSize: 8,
+        });
+        this.house3 = new House({
+            x: 1200,
+            y: 300,
+            w: 50,
+            h: 80,
+            red: 28,
+            green: 123,
+            blue: 138,
+            doorKnobSize: 8,
+        });
+        this.creature = new Creature({
+            x: 1000,
+            y: 100,
+            w: 30,
+            h: 60
+        });
     }
 
+
+    // Displays the objects
     draw() {
         background(186, 239, 158);
 
-        // Call the state's methods to make the animation work
-        this.move();
-        this.display();
-        this.keyPressed(keyCode);
-        this.keyReleased(keyCode);
-        this.checkEnding();
-    }
+        this.checkEndings(this.player2, this.creature);
 
-    // Gives movement to the player
-    move() {
-        this.player.x += this.player.vx;
-        this.player.y += this.player.vy;
-    }
-
-    // Displays the player
-    display() {
+        // Draws the house with all its functions
         push();
-        noStroke();
-        fill(this.player.r, this.player.g, this.player.b);
-        rectMode(CENTER);
-        rect(this.player.x, this.player.y, this.player.w, this.player.h);
+        this.house.display();
+        pop();
+
+        // Draws the house2 with all its functions
+        push();
+        this.house2.display();
+        pop();
+
+        // Draws the house3 with all its functions
+        push();
+        this.house3.display();
+        pop();
+
+        // Draws the player with all its functions
+        push();
+        this.player.move();
+        this.player.display();
+        pop();
+
+        // Draws the player2 with all its functions
+        push();
+        this.player2.move();
+        this.player2.display();
+        pop();
+
+        // Draws the creature with all its functions
+        push();
+        this.creature.move(this.player);
+        this.creature.checkSides();
+        this.creature.display();
         pop();
     }
 
-    // Starts the player when the arrow keys are pressed
+    checkEndings() {
+        let d = dist(this.player2.x, this.player2.y, this.creature.x, this.creature.y);
+        if (d < this.player2.size / 2 + this.creature.size / 2) {
+            // this is not an actual class yet.
+            currentState = new Note1();
+        }
+    }
+
+    mousePressed() {
+        //Making everything mousePressed() in the House class be called
+        house.mousePressed();
+        house2.mousePressed();
+        house3.mousePressed();
+    }
+
     keyPressed(keyCode) {
-        if (keyCode === this.player.leftKey) {
-            this.player.vx = -this.player.speed;
-        }
-        else if (keyCode === this.player.rightKey) {
-            this.player.vx = this.player.speed;
-        }
-
-        if (keyCode === this.player.upKey) {
-            this.player.vy = -this.player.speed;
-        }
-        else if (keyCode === this.player.downKey) {
-            this.player.vy = this.player.speed;
-        }
+        this.player.keyPressed(keyCode);
+        this.player2.keyPressed(keyCode);
     }
 
-    // Stops the player when the arrow keys are released
     keyReleased(keyCode) {
-        if (keyCode === this.player.leftKey && this.player.vx < 0) {
-            this.player.vx = 0;
-        }
-        else if (keyCode === this.player.rightKey && this.player.vx > 0) {
-            this.player.vx = 0;
-        }
-
-        if (keyCode === this.player.upKey && this.player.vy < 0) {
-            this.player.vy = 0;
-        }
-        else if (keyCode === this.player.downKey && this.player.vy > 0) {
-            this.player.vy = 0
-        }
-    }
-
-    checkEnding() {
-        let d = dist(player2.x, player2.y, creature.x, creature.y);
-        if (d < player2.size / 2 + creature.size / 2) {
-            state = `note`;
-            currentState = new Ending();
-        }
+        this.player.keyReleased(keyCode);
+        this.player2.keyReleased(keyCode);
     }
 }
---
+++ projects/project2/js/Note1.js
@@ -0,0 +1,33 @@
+class Note1 {
+    constructor() {
+        this.Note1String = "Dear Evelyn,\n\n I remember the first time you held me in your arms.\n I remember looking up at you as you held me, I remember the warmth, and feeling at peace.\n I felt happy, for the first time I knew I felt happy.\n I remember looking up as you stared down at me while holding me, you looked so beautiful.\n That was a really great day.\n Wasn't it?\n\n Sincerely,\n Her";
+        this.Note1String2 = "(Press Any Key to Go to Level 2)";
+    }
+
+    draw() {
+        background(236, 204, 74);
+
+        this.displayNote1();
+    }
+
+    displayNote1() {
+        push();
+        textSize(32);
+        fill(0);
+        textAlign(LEFT, CENTER);
+        text(this.Note1String, width / 80, height / 2);
+        textSize(20);
+        fill(102, 107, 131);
+        textAlign(CENTER, CENTER);
+        text(this.Note1String2, width / 2, 570);
+        pop();
+    }
+
+    mousePressed() {
+
+    }
+
+    keyPressed() {
+        currentState = new Level2();
+    }
+}
