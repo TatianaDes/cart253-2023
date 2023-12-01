5e4c295b03bed163f0326a4a8d34be72881ed388
TatianaDes <tatiana.d0714@gmail.com>
2023-12-01 17:14:35 -0500

    P2: Made realization able to chase the player and run away from player2

--- projects/project2/js/Realization.js
+++ projects/project2/js/Realization.js
@@ -9,7 +9,8 @@ class Realization {
         this.size = 40;
 
         this.acceleration = 1;
-        this.maxSpeed = 2;
+        this.maxAcceleration = 2;
+        this.maxSpeed = 3;
         this.vx = 0;
         this.vy = 0;
         this.ax = 0;
@@ -18,53 +19,84 @@ class Realization {
 
     // Gives movement to the creature
     move(player, player2) {
+        // Main insight: calculate the player-based accelerations
+        // first, and only apply them afterwards. That allows for
+        // the idea that they might contribute zero acceleration (if out
+        // of range for instance)
+
+        // Assume no acceleration
+        let player2Acceleration = {
+            x: 0,
+            y: 0
+        };
+
         let player2Distance = dist(player2.x, player2.y, this.x, this.y);
-        let player2Acceleration = map(player2Distance, 0, 400, 0, this.acceleration);
-        if (player2.x < this.x) {
-            this.ax += player2Acceleration;
-        }
-        else {
-            this.ax -= player2Acceleration;
+        // Only apply acceleration if the player2 is within range
+        if (player2Distance < 300) {
+            let acceleration = map(player2Distance, 0, 400, 0, this.acceleration);
+            if (player2.x < this.x) {
+                player2Acceleration.x = acceleration;
+            }
+            else {
+                player2Acceleration.x = -acceleration;
+            }
+
+            if (player2.y < this.y) {
+                player2Acceleration.y = acceleration;
+            }
+            else {
+                player2Acceleration.y = -acceleration;
+            }
         }
 
-        if (player2.y < this.y) {
-            this.ay += player2Acceleration;
-        }
-        else {
-            this.ay -= player2Acceleration;
+        // Assume no acceleration
+        let playerAcceleration = {
+            x: 0,
+            y: 0
         }
 
         let playerDistance = dist(player.x, player.y, this.x, this.y);
-        let playerAcceleration = map(playerDistance, 0, 400, -this.acceleration, 0);
-        if (player2.x < this.x) {
-            this.ax += playerAcceleration;
-        }
-        else {
-            this.ax -= playerAcceleration;
+        // Only apply acceleration if the player2 is within range
+        if (playerDistance < 400) {
+            let acceleration = map(playerDistance, 0, 600, 0, this.acceleration);
+            if (player.x < this.x) {
+                playerAcceleration.x = -acceleration;
+            }
+            else {
+                playerAcceleration.x = acceleration;
+            }
+
+            if (player.y < this.y) {
+                playerAcceleration.y = -acceleration;
+            }
+            else {
+                playerAcceleration.y = acceleration;
+            }
         }
 
-        if (player2.y < this.y) {
-            this.ay += playerAcceleration;
-        }
-        else {
-            this.ay -= playerAcceleration;
-        }
+        // Apply the acceleration
+        this.ax = playerAcceleration.x + player2Acceleration.x;
+        this.ay = playerAcceleration.y + player2Acceleration.y;
 
-
-        this.ax = constrain(this.ax, -this.acceleration, this.acceleration);
-        this.ay = constrain(this.ay, -this.acceleration, this.acceleration);
-
+        // Apply current acceleration
         this.vx += this.ax;
         this.vy += this.ay;
+
+        // Apply drag
+        this.vx *= 0.9;
+        this.vy *= 0.9;
 
         // Allows for movement with acceleration and velocity to make the movement stay at a certain speed
         this.vx = constrain(this.vx, -this.maxSpeed, this.maxSpeed);
         this.vy = constrain(this.vy, -this.maxSpeed, this.maxSpeed);
 
+        // Reduce velocity to 0 if it's close
+        if (abs(this.vx) < 0.1) this.vx = 0;
+        if (abs(this.vy) < 0.1) this.vy = 0;
+
         // Allows the creature to move
         this.x = this.x + this.vx;
         this.y = this.y + this.vy;
-
     }
 
     // Constrains the creature from the borders of the canvas
