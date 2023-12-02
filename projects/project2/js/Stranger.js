1a4c61ba6a40f1c96475afda66e6d3455dcd30ca
TatianaDes <tatiana.d0714@gmail.com>
2023-12-02 16:35:12 -0500

    P2: Realizing that I like my code better this way

--- projects/project2/js/Stranger.js
+++ projects/project2/js/Stranger.js
@@ -1,6 +1,6 @@
 class Stranger {
 
-    constructor(x, y, red, green, blue, /*note*/) {
+    constructor(x, y, red, green, blue) {
         this.x = x;
         this.y = y;
         this.size = 50;
@@ -10,17 +10,6 @@ class Stranger {
         this.speed = 3;
         this.vx = random(-this.speed, this.speed);
         this.vy = random(-this.speed, this.speed);
-
-        // // Oscillator
-        // this.oscillator = new p5.Oscillator();
-        // this.nearFreq = 220;
-        // this.farFreq = 440;
-        // this.oscillator.amp(0.025);
-        // this.oscillator.start();
-
-        // // Synth
-        // this.note = note;
-        // this.synth = new p5.PolySynth();
     }
 
     move() {
@@ -35,32 +24,7 @@ class Stranger {
 
         this.x = constrain(this.x, 0, width);
         this.y = constrain(this.y, 0, height);
-
-        // this.x += this.vx;
-        // this.y += this.vy;
-
-        // // Update frequency
-        // let d = dist(this.x, this.y, width / 2, height / 2);
-        // let maxDist = dist(0, 0, width / 2, height / 2);
-        // let newFreq = map(d, 0, maxDist, this.nearFreq, this.farFreq);
-        // this.oscillator.freq(newFreq);
     }
-
-    // bounce() {
-    //     if (this.x - this.size / 2 < 0 || this.x + this.size / 2 > width) {
-    //         this.vx = -this.vx;
-    //         this.playNote();
-    //     }
-
-    //     if (this.y - this.size / 2 < 0 || this.y + this.size / 2 > height) {
-    //         this.vy = -this.vy;
-    //         this.playNote();
-    //     }
-    // }
-
-    // playNote() {
-    //     this.synth.play(this.note, 0.4, 0, 0.1);
-    // }
 
     display() {
         push();
