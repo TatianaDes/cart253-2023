e68521a33fb4914bfe64267fc247572324f4966b
TatianaDes <tatiana.d0714@gmail.com>
2023-12-05 16:30:15 -0500

    P2: Changed some words and fonts

--- projects/project2/js/Note2.js
+++ projects/project2/js/Note2.js
@@ -5,7 +5,7 @@ class Note2 {
     // Creating dimensions of the objects
     constructor() {
         // Adding word strings to the Note2 screen
-        this.Note2String = "Dear Evelyn,\n\n I know I said I really liked you, I know I kissed your hands,\n and I know I said yes to being with you. I love you, I really do.\n I love the way you love me, because you really love me.\n I remember being excited to talk about you to my family,\n I told everyone.\n But when they asked who you were to me... I froze.\n You are who I wanted... Right?\n\n Sincerely, Her";
+        this.Note2String = "Dear Evelyn,\n\n I know I said I really liked you, I know I kissed your hands,\n and I know I said yes to being with you. I love you, I really do.\n I love the way you love me, because you really love me.\n I remember being excited to talk about you to my family,\n I told everyone.\n But when they asked who you were to me... I froze.\n You are who I wanted... Right?\n\n Sincerely,\n Her";
         this.Note2String2 = "(Press Any Key to Go to Level 3)";
     }
 
--- projects/project2/js/Note4.js
+++ projects/project2/js/Note4.js
@@ -5,7 +5,7 @@ class Note4 {
     // Creating dimensions of the objects
     constructor() {
         // Adding word strings to the Note4 screen
-        this.Note4String = "Dear Evelyn,\n\n I know after all this it is wrong for me to write to you.\n All the pain I caused you, all because of my feelings for him.\n I know I lied to you and I know when you treated me so right,\n I treated you the worst I could. You deserve better.\n You deserve someone who will love you\n and who will see you the way I see you.\n As someone who is kind, intelligent and so unbelievably caring.\n I really do love you, you know,\n I loved you as much as I can love anyone,\n but it is never enough.\n I do miss you and care about you,\n and I always will reserve part of my heart for you.\n\n Sincerely,\n Her ";
+        this.Note4String = "Dear Evelyn,\n\n I know after all this it is wrong for me to write to you.\n All the pain I caused you, all because of my feelings for him.\n I know I lied to you and I know when you treated me so right,\n I treated you the worst I could. You deserve better.\n You deserve someone who will love you\n and who will see you the way I see you.\n As someone who is kind, intelligent and so unbelievably caring.\n I really do love you, you know,\n I love you as much as I can love anyone,\n but it is never enough.\n I do miss you and care about you,\n and I always will reserve part of my heart for you.\n\n Sincerely,\n Her ";
         this.Note4String2 = "(Press Any Key to Go Back to the Title)";
     }
 
